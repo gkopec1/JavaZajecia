236d596e94da177c717278851aacffcaf04ff488
gkopec1 <118059023+gkopec1@users.noreply.github.com>
2022-11-12 19:19:02 +0100

    Add files via upload

---
+++ Zmienne_gra.java
@@ -0,0 +1,15 @@
+package zajecia;
+
+public class Zmienne_gra {
+    private int zycie;
+    private int exp;
+    private int obrazenia;
+
+    public Zmienne_gra() {
+    this.zycie=a;
+    this.exp=100;
+    this.obrazenia=0;
+
+
+    }
+}
---
+++ Zycie_i_walka.java
@@ -0,0 +1,34 @@
+package zajecia;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Zycie_i_walka {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Zycie_i_walka a;
+        while (true) {
+            System.out.print("Wybierz opcje:");
+
+            String opcja = scanner.nextLine();
+            if (opcja.equals("wyjscie")) {
+                break;
+            }
+
+
+            if (opcja.equals("walka")) {
+
+            }
+            if (opcja.equals("statystyki")) {
+                System.out.print();
+            }
+            if (opcja.equals("leczenie")) {
+                int zdrowie = 100;
+                System.out.print(a.zycie + zdrowie);
+            }
+        }
+    }
+}
+
+
+
